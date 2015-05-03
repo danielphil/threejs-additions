5ee365f5ac8a7daac54706e37c53e371cbde8ef3
Daniel Phillips <dphildev@gmail.com>
2015-05-03 18:22:23 +0100

    Committing WIP before I refactor the tests a little.

---
+++ additions.js
@@ -0,0 +1,98 @@
+var additions = additions || {};
+
+(function () {
+    additions.Matrix4 = function (threejsMatrix) {
+        if (arguments.length > 1) {
+            // If we've got more than one argument, set the values of the matrix directly
+            this.matrix = new THREE.Matrix4();
+            this.matrix.set.apply(this.matrix, arguments)
+        } else if (threejsMatrix) {
+            // Initialise from the three matrix
+            this.matrix = newCopy(threejsMatrix);
+        } else {
+            // Set to identity otherwise
+            this.matrix = new THREE.Matrix4();
+        }
+
+        this.elements = this.matrix.elements;
+    };
+
+    function newCopy(m) {
+        return (new THREE.Matrix4()).copy(m);
+    }
+
+    // Functions that can be called directly because they don't modify the matrix
+    var passthroughFunctions = [
+        "extractBasis",
+        "applyToVector3Array",
+        "determinant",
+        "flattenToArrayOffset",
+        "getPosition",
+        "getMaxScaleOnAxis",
+        "decompose",
+        "toArray"
+    ];
+    _(passthroughFunctions).each(function (name) {
+        additions.Matrix4.prototype[name] = function () {
+            return this.matrix[name].apply(this.matrix, arguments);
+        }
+    });
+
+    // Functions which don't require a source matrix
+    var standaloneFunctions = [
+        "identity",
+        "makeBasis",
+        "makeRotationFromEuler",
+        "setRotationFromQuaternion",
+        "makeRotationFromQuaternion",
+        "lookAt",
+        "multiplyMatrices",
+        "multiplyToArray",
+        "makeTranslation",
+        "makeRotationX",
+        "makeRotationY",
+        "makeRotationZ",
+        "makeRotationAxis",
+        "makeScale",
+        "compose",
+        "makeFrustum",
+        "makePerspective",
+        "makeOrthographic",
+        "fromArray"
+    ];
+    _(standaloneFunctions).each(function (name) {
+        additions.Matrix4[name] = function () {
+            var newMatrix = new THREE.Matrix4();
+            newMatrix[name].apply(newMatrix, arguments);
+            return new additions.Matrix4(newMatrix);
+        }
+    });
+
+    // Functions that include the current matrix as an input
+    var newMatrixFunctions = [
+        "extractPosition",
+        "copyPosition",
+        "extractRotation",
+        "multiply",
+        "multiplyScalar",
+        "transpose",
+        "setPosition",
+        "scale"
+    ];
+    _(newMatrixFunctions).each(function (name) {
+        additions.Matrix4.prototype[name] = function () {
+            var newMatrix = newCopy(this.matrix);
+            newMatrix[name].apply(this.newMatrix, arguments);
+            return new additions.Matrix4(newMatrix);
+        }
+    });
+
+    // Special case for matrix inverse
+    additions.Matrix4.prototype.getInverse = function (other) {
+        if (other instanceof additions.Matrix4) {
+            other = other.matrix;
+        }
+
+        return new additions.Matrix4((new THREE.Matrix4()).getInverse(other));
+    };
+})();
---
+++ tests.js
@@ -0,0 +1,108 @@
+QUnit.module("Matrix4");
+
+QUnit.test("Creation", function(assert) {
+    var identity = new THREE.Matrix4();
+
+    assert.deepEqual(
+        new additions.Matrix4().elements,
+        identity.elements,
+        "Matrix should be initialised to identity on creation"
+    );
+
+    var nonZero = new THREE.Matrix4();
+    nonZero.set(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+    var additionsMatrix = new additions.Matrix4(nonZero);
+    assert.deepEqual(
+        additionsMatrix.elements,
+        nonZero.elements,
+        "Should be possible to initialise Matrix from an existing three.js matrix"
+    );
+
+    var nonZeroCopy = new THREE.Matrix4();
+    nonZeroCopy.copy(nonZero);
+    nonZero.identity();
+    assert.deepEqual(
+        additionsMatrix.elements,
+        nonZeroCopy.elements,
+        "Modifying the source three.js matrix after creation should not modify the additions.Matrix4"
+    );    
+});
+
+QUnit.test("extractBasis", function(assert) {
+    var threeExpected = new THREE.Matrix4();
+    threeExpected.set(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
+    var unmodifiedExpected = new THREE.Matrix4();
+    unmodifiedExpected.copy(threeExpected);
+
+    var additionsMatrix = new additions.Matrix4(threeExpected);
+
+    var xAxisExpected = new THREE.Vector3();
+    var yAxisExpected = new THREE.Vector3();
+    var zAxisExpected = new THREE.Vector3();
+
+    var xAxis = new THREE.Vector3();
+    var yAxis = new THREE.Vector3();
+    var zAxis = new THREE.Vector3();
+
+    threeExpected.extractBasis(xAxisExpected, yAxisExpected, zAxisExpected);
+    additionsMatrix.extractBasis(xAxis, yAxis, zAxis);
+
+    assert.deepEqual(
+        additionsMatrix.elements,
+        unmodifiedExpected.elements,
+        "Matrix should not be modified when extractBasis called"
+    );
+
+    assert.deepEqual(vectorAsArray(xAxis), vectorAsArray(xAxisExpected), "xAxis should match expected");
+    assert.deepEqual(vectorAsArray(yAxis), vectorAsArray(yAxisExpected), "yAxis should match expected");
+    assert.deepEqual(vectorAsArray(zAxis), vectorAsArray(zAxisExpected), "zAxis should match expected");
+});
+
+QUnit.test("applyToVector3Array", function(assert) {
+    var threeExpected = new THREE.Matrix4();
+    threeExpected.set(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
+    var unmodifiedExpected = new THREE.Matrix4();
+    unmodifiedExpected.copy(threeExpected);
+
+    var additionsMatrix = new additions.Matrix4(threeExpected);
+
+    var expectedVec = [1, 1, 1];
+    var vec = [1, 1, 1];
+    threeExpected.applyToVector3Array(expectedVec);
+    additionsMatrix.applyToVector3Array(vec);
+
+    assert.deepEqual(
+        additionsMatrix.elements,
+        unmodifiedExpected.elements,
+        "Matrix should not be modified when extractBasis called"
+    );
+
+    assert.deepEqual(vec, expectedVec, "applyToVector3Array behaviour should match THREE.Matrix4");
+});
+
+QUnit.test("determinant", function(assert) {
+    var threeExpected = new THREE.Matrix4();
+    threeExpected.set(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
+    var unmodifiedExpected = new THREE.Matrix4();
+    unmodifiedExpected.copy(threeExpected);
+
+    var additionsMatrix = new additions.Matrix4(threeExpected);
+
+    var expectedDet = threeExpected.determinant();
+    var det = additionsMatrix.determinant();  
+
+    assert.deepEqual(
+        additionsMatrix.elements,
+        unmodifiedExpected.elements,
+        "Matrix should not be modified when determinant called"
+    );
+
+    assert.deepEqual(det, expectedDet, "determinant behaviour should match THREE.Matrix4");
+});
+
+function vectorAsArray(vector) {
+    return [vector.x, vector.y, vector.z];
+}
